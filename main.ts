303cc91903b8c2d131a50f55938acab274c45ff8
liuxspro <liuxspro@gmail.com>
2024-10-25 08:47:15 +0800

    remove got

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
 import * as wasm from "./pkg/webp_to_png.js";
-import got from "npm:got";
 import { create_Capabilities } from "./wmts.ts";
 
-async function get_tile(z: string, x: string, y: string, mk: string, tk: string) {
+async function get_tile(
+  z: string,
+  x: string,
+  y: string,
+  mk: string,
+  tk: string
+) {
   // 通过添加sch=wmts可返回正常XYZ顺序, 否则使用Math.pow(2, z) - 1 - y计算-y值
   const tile_url = `https://api.jl1mall.com/getMap/${z}/${x}/${y}?mk=${mk}&tk=${tk}&sch=wmts`;
-  const tile_data = await got(tile_url).buffer();
+  const tile_data = await (await fetch(tile_url)).bytes();
   // webp to png
   const pngBuffer = wasm.webp_to_png(tile_data);
   return pngBuffer;
@@ -16,7 +21,7 @@ async function get_tile(z: string, x: string, y: string, mk: string, tk: string)
 async function get_tile_earth(z: string, x: string, y: string) {
   const token = "Bearer%20a84a40c81f784490a4c5689187054abf";
   const tile_url = `https://tile.charmingglobe.com/tile/china2023_5_shield/wmts/${z}/${x}/${y}?v=v1&token=${token}`;
-  const tile_data = await got(tile_url).buffer();
+  const tile_data = await (await fetch(tile_url)).bytes();
   const pngBuffer = wasm.webp_to_png(tile_data);
   return pngBuffer;
 }
@@ -50,6 +55,21 @@ router.get("/", (ctx) => {
 router.get("/wmts/jl1earth", (ctx) => {
   const domain = "jl1.deno.dev";
   const template = `https://${domain}/jl1earth/{TileMatrix}/{TileCol}/{TileRow}`;
+  ctx.response.type = "text/xml;charset=UTF-8";
+  ctx.response.body = create_Capabilities(template);
+});
+
+router.get("/wmts/jl1", (ctx) => {
+  // 获取查询参数
+  const url = new URL(ctx.request.url.toString());
+  const mk = url.searchParams.get("mk") || "73ad26c4aa6957eef051ecc5a15308b4";
+  const tk = url.searchParams.get("tk") || "";
+  if (tk == "") {
+    ctx.response.body = "tk is needed";
+    return;
+  }
+  const domain = "jl1.deno.dev";
+  const template = `https://${domain}/jl1/{TileMatrix}/{TileCol}/{TileRow}?mk=${mk}&amp;tk=${tk}`;
   ctx.response.type = "text/xml;charset=UTF-8";
   ctx.response.body = create_Capabilities(template);
 });
